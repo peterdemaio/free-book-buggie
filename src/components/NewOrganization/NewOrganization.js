bba4698dbe93f88ce8aa6797620a9ba7b0466ce5
Nic Payne <nic.r.payne@gmail.com>
2020-03-06 12:16:51 -0600

    inputs working and values being set to local state

--- src/components/NewOrganization/NewOrganization.js
+++ src/components/NewOrganization/NewOrganization.js
@@ -20,102 +20,14 @@ class NewOrganization extends Component {
         }
     }
 
-    handleNameChangeFor = (event) => {
-        console.log(event.target.value)
+    handleChangeFor = (event, propertyName) => {
+        console.log(event.target.value, propertyName);
         this.setState({
             newEntry: {
                 ...this.state.newEntry,
-                name: this.state.name  
-            }
-        })
-    }
-    handleLogoChangeFor = (event) => {
-        console.log(event.target.value)
-        this.setState({
-            newEntry: {
-                ...this.state.newEntry,
-                logo: this.state.logo
-            }
-        })
-    }
-    handleTypeChangeFor = (event) => {
-        console.log(event.target.value)
-        this.setState({
-            newEntry: {
-                ...this.state.newEntry,
-                type: this.state.type
-            }
-        })
-    }
-    handleAddressNumberChangeFor = (event) => {
-        console.log(event.target.value)
-        this.setState({
-            newEntry: {
-                ...this.state.newEntry,
-                address_number: this.state.address_number
-            }
-        })
-    }
-    handleAddressStreetChangeFor = (event) => {
-        console.log(event.target.value)
-        this.setState({
-            newEntry: {
-                ...this.state.newEntry,
-                address_street: this.state.address_street
-            }
-        })
-    }
-    handleAddressUnitChangeFor = (event) => {
-        console.log(event.target.value)
-        this.setState({
-            newEntry: {
-                ...this.state.newEntry,
-                address_unit: this.state.address_unit
-            }
-        })
-    }
-    handleCityChangeFor = (event) => {
-        console.log(event.target.value)
-        this.setState({
-            newEntry: {
-                ...this.state.newEntry,
-                state: this.state.city
-            }
-        })
-    }
-    handleStateChangeFor = (event) => {
-        console.log(event.target.value)
-        this.setState({
-            newEntry: {
-                ...this.state.newEntry,
-                city: this.state.state
-            }
-        })
-    }
-    handleZipChangeFor = (event) => {
-        console.log(event.target.value)
-        this.setState({
-            newEntry: {
-                ...this.state.newEntry,
-                zip: this.state.zip
-            }
-        })
-    }
-    handleCountyChangeFor = (event) => {
-        console.log(event.target.value)
-        this.setState({
-            newEntry: {
-                ...this.state.newEntry,
-                county: this.state.county
-            }
-        })
-    }
-    handleNotesChangeFor = (event) => {
-        console.log(event.target.value)
-        this.setState({
-            newEntry: {
-                ...this.state.newEntry,
-                notes: this.state.notes
+                [propertyName]: event.target.value
+             
+
             }
         })
     }
@@ -136,45 +48,46 @@ class NewOrganization extends Component {
                     <h1>Add New Organization</h1>
                     <button onClick={() => this.props.history.push('/collectBooks')}>Save</button>
                     <button onClick={() => this.props.history.push('/collectBooks')}>Cancel</button>
-
+                    <form>
                     <input
                     placeholder="Name of Organization"
-                    onChange={(event) => this.handleNameChangeFor(event)} />
+                    onChange={(event) => this.handleChangeFor(event, 'name')} />
                     <input
                     placeholder="Organization Logo Link"
-                    onChange={(event) => this.handleLogoChangeFor(event)} />
+                    onChange={(event) => this.handleChangeFor(event, 'logo')} />
                     <input
                     placeholder="Non-Profit, School, etc."
-                    onChange={(event) => this.handleTypeChangeFor(event)} />
+                    onChange={(event) => this.handleChangeFor(event, 'type')} />
                     <input
                     placeholder="Address Number"
-                    onChange={(event) => this.handleAddressNumberChangeFor(event)} />
+                    onChange={(event) => this.handleChangeFor(event, 'number')} />
                     <input
                     placeholder="Address Street"
-                    onChange={(event) => this.handleAddressStreetChangeFor(event)} />
+                    onChange={(event) => this.handleChangeFor(event, 'street')} />
                     <input
                     placeholder="Unit"
-                    onChange={(event) => this.handleAddressUnitChangeFor(event)} />
+                    onChange={(event) => this.handleChangeFor(event, 'unit')} />
                     <input
                     placeholder="City"
-                    onChange={(event) => this.handleCityChangeFor(event)} />
+                    onChange={(event) => this.handleChangeFor(event, 'city')} />
                     <input
                     placeholder="State"
-                    onChange={(event) => this.handleStateChangeFor(event)} />
+                    onChange={(event) => this.handleChangeFor(event, 'state')} />
                     <input
                     placeholder="Zip"
-                    onChange={(event) => this.handleZipChangeFor(event)} />
+                    onChange={(event) => this.handleChangeFor(event, 'zip')} />
                     <input
                     placeholder="County"
-                    onChange={(event) => this.handleCountyChangeFor(event)} />
+                    onChange={(event) => this.handleChangeFor(event, 'county')} />
                     <textfield
                     placeholder="Notes"
-                    onChange={(event) => this.handleNotesChangeFor(event)} />
+                    onChange={(event) => this.handleChangeFor(event, 'notes')} />
 
                     <br />
                     <button className='submit-button'
                     onClick={this.handleClick}>
-                        Add </button>
+                    Add </button>
+                    </form>
                 </div>
             </>     
         )
