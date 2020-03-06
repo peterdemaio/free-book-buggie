db7b8f708046a93adf35724506869e1733910cf5
AndyMcElligott <andymc2315@gmail.com>
2020-03-06 17:00:39 -0600

    btns added with MUI

--- src/components/CollectBooks/CollectBooks.js
+++ src/components/CollectBooks/CollectBooks.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+// Material UI imports
+import {
+    withStyles,
+    Button,
+    Grid
+}
+    from '@material-ui/core';
+
+const styles = theme => ({
+    button: {
+        margin: theme.spacing.unit,
+    },
+    input: {
+        display: 'none',
+    },
+});
+
+
 class CollectBooks extends Component {
 
 
@@ -9,6 +27,36 @@ class CollectBooks extends Component {
         return (
             <>
                 <h1>CollectBooks page</h1>
+                <Grid container
+                    direction="column"
+                    justify="flex-end"
+                    alignItems="flex-end"
+                >
+                    <Button container
+                        color="primary"
+                        className={this.props.classes.button}
+                        onClick={() => this.props.history.push('/newOrganization')}
+                    >
+                        New Organization
+                    </Button>
+                    <br />
+                    <Button
+                        color="primary"
+                        className={this.props.classes.button}
+                        onClick={() => this.props.history.push('/newEvent')}
+                    >
+                        New Event
+                    </Button>
+                    <br />
+                    <Button
+                        color="primary"
+                        className={this.props.classes.button}
+                        onClick={() => this.props.history.push('/newIndividual')}
+                    >
+                        New Individual
+                    </Button>
+                </Grid>
+
                 <button onClick={() => this.props.history.push('/editOrganization')}>Sample Organization</button>
                 <button onClick={() => this.props.history.push('/newOrganization')}>Add New Organization</button>
                 <button onClick={() => this.props.history.push('/home')}>Home</button>
@@ -17,4 +65,8 @@ class CollectBooks extends Component {
     }
 }
 
-export default CollectBooks;
+const mapStateToProps = state => ({
+    errors: state.errors,
+});
+
+export default connect(mapStateToProps)(withStyles(styles)(CollectBooks));
