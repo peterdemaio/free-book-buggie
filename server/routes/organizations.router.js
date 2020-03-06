44c6e60ec13baba550660270e4d6fdc70a9560d7
Nic Payne <nic.r.payne@gmail.com>
2020-03-06 15:19:38 -0600

    completed post route with saga

--- server/routes/organizations.router.js
+++ server/routes/organizations.router.js
@@ -16,4 +16,34 @@ router.get('/', (req, res) => {
         })
 })
 
+router.post('/', rejectUnauthenticated, (req, res) => {
+    console.log('in organizations post router')
+    const newEntry = req.body;
+    console.log(newEntry)
+    const queryText = `INSERT INTO "organizations" ("name", "logo", "type", 
+                            "address_number", "address_street", "address_unit", "city",
+                            "state", "county", "zip", "notes") 
+                            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);`;
+    const queryValues = [
+        newEntry.name,
+        newEntry.logo,
+        newEntry.type,
+        newEntry.address_number,
+        newEntry.address_unit,
+        newEntry.city,
+        newEntry.state,
+        newEntry.county,
+        newEntry.zip,
+        newEntry.notes
+    ];
+    pool.query(queryText, queryValues)
+        .then(() => {
+            res.sendStatus(201);
+            console.log(queryValues)
+        }).catch((err) => {
+            console.log('Error in router.post on entry router', err);
+            res.sendStatus(500);
+        })
+});
+
 module.exports = router;
--- src/components/NewOrganization/NewOrganization.js
+++ src/components/NewOrganization/NewOrganization.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+
 
 class NewOrganization extends Component {
 
@@ -45,48 +45,51 @@ class NewOrganization extends Component {
         return (
             <>
                 <div>
-                    <h1>Add New Organization</h1>
-                    <button onClick={() => this.props.history.push('/collectBooks')}>Save</button>
-                    <button onClick={() => this.props.history.push('/collectBooks')}>Cancel</button>
-                    <form>
-                    <input
-                    placeholder="Name of Organization"
-                    onChange={(event) => this.handleChangeFor(event, 'name')} />
-                    <input
-                    placeholder="Organization Logo Link"
-                    onChange={(event) => this.handleChangeFor(event, 'logo')} />
-                    <input
-                    placeholder="Non-Profit, School, etc."
-                    onChange={(event) => this.handleChangeFor(event, 'type')} />
-                    <input
-                    placeholder="Address Number"
-                    onChange={(event) => this.handleChangeFor(event, 'number')} />
-                    <input
-                    placeholder="Address Street"
-                    onChange={(event) => this.handleChangeFor(event, 'street')} />
-                    <input
-                    placeholder="Unit"
-                    onChange={(event) => this.handleChangeFor(event, 'unit')} />
-                    <input
-                    placeholder="City"
-                    onChange={(event) => this.handleChangeFor(event, 'city')} />
-                    <input
-                    placeholder="State"
-                    onChange={(event) => this.handleChangeFor(event, 'state')} />
-                    <input
-                    placeholder="Zip"
-                    onChange={(event) => this.handleChangeFor(event, 'zip')} />
-                    <input
-                    placeholder="County"
-                    onChange={(event) => this.handleChangeFor(event, 'county')} />
-                    <textfield
-                    placeholder="Notes"
-                    onChange={(event) => this.handleChangeFor(event, 'notes')} />
+                    <h1 align="center">Add New Organization</h1>
+                    {/* <button onClick={() => this.props.history.push('/collectBooks')}>Add</button>
+                    <button onClick={() => this.props.history.push('/collectBooks')}>Cancel</button> */}
+                    <form >
+                        <input
+                            placeholder="Name of Organization"
+                            onChange={(event) => this.handleChangeFor(event, 'name')} />
+                        <input
+                            placeholder="Organization Logo Link"
+                            onChange={(event) => this.handleChangeFor(event, 'logo')} />
+                        <input
+                            placeholder="Non-Profit, School, etc."
+                            onChange={(event) => this.handleChangeFor(event, 'type')} />
+                        <input
+                            placeholder="Address Number"
+                            onChange={(event) => this.handleChangeFor(event, 'number')} />
+                        <input
+                            placeholder="Address Street"
+                            onChange={(event) => this.handleChangeFor(event, 'street')} />
+                        <input
+                            placeholder="Unit"
+                            onChange={(event) => this.handleChangeFor(event, 'unit')} />
+                        <input
+                            placeholder="City"
+                            onChange={(event) => this.handleChangeFor(event, 'city')} />
+                        <input
+                            placeholder="State"
+                            onChange={(event) => this.handleChangeFor(event, 'state')} />
+                        <input
+                            placeholder="Zip"
+                            onChange={(event) => this.handleChangeFor(event, 'zip')} />
+                        <input
+                            placeholder="County"
+                            onChange={(event) => this.handleChangeFor(event, 'county')} />
+                        <textarea
+                            placeholder="Notes"
+                            rows="6" cols="50"
+                            onChange={(event) => this.handleChangeFor(event, 'notes')}
+                        />
 
                     <br />
                     <button className='submit-button'
                     onClick={this.handleClick}>
                     Add </button>
+                    <br/>
                     </form>
                 </div>
             </>     
