1ff80eaba21bd7ebd6be7dd7699e474d68f549b1
AndyMcElligott <andymc2315@gmail.com>
2020-03-09 10:23:50 -0500

    imported org search and map

--- src/components/CollectBooks/CollectBooks.js
+++ src/components/CollectBooks/CollectBooks.js
@@ -23,54 +23,82 @@ const styles = theme => ({
 
 class CollectBooks extends Component {
 
-    let id = this.props.id
-    let index = id -1
-    let organization = this.props.reduxStore.organization[index]
+    // let id = this.props.id
+    // let index = id - 1
+    // let organization = this.props.reduxStore.organization[index]
+
+    componentDidMount() {
+        console.log('organizations page ready')
+        this.props.dispatch({
+            type: 'GET_ORGANIZATIONS'
+        })
+    }
 
     render() {
         return (
             <>
                 <h1>CollectBooks page</h1>
-                <Grid container
-                    direction="column"
-                    justify="flex-end"
-                    alignItems="flex-end"
-                >
-                    <Button container
-                        color="primary"
-                        className={this.props.classes.button}
-                        onClick={() => this.props.history.push('/newOrganization')}
+                {/* Search functionality for orgs, make clickable to display info */}
+                <div >
+                    <input placeholder="Search for organization"
+                        onChange={(event) => this.props.dispatch({
+                            type: 'SEARCH_ORGANIZATIONS',
+                            payload: event.target.value
+                        })}>
+                    </input>
+                </div>
+                <div>
+                    <Grid container
+                        // direction="column"
+                        // justify="space-evenly"
+                        // alignItems="center"
+                        spacing={12}
                     >
-                        New Organization
-                    </Button>
-                    <br />
-                    <Button
-                        color="primary"
-                        className={this.props.classes.button}
-                        onClick={() => this.props.history.push('/newEvent')}
-                    >
-                        New Event
-                    </Button>
-                    <br />
-                    <Button
-                        color="primary"
-                        className={this.props.classes.button}
-                        onClick={() => this.props.history.push('/newIndividual')}
-                    >
-                        New Individual
-                    </Button>
-                </Grid>
-
+                        {this.props.reduxStore.organizations.map(org =>
+                            <OrganizationsListItem key={org.id} org={org} />
+                        )}
+                        <Grid item
+                            direction="column"
+                            justify="flex-end"
+                            alignItems="flex-end"
+                        >
+                            <Button container
+                                color="primary"
+                                className={this.props.classes.button}
+                                onClick={() => this.props.history.push('/newOrganization')}
+                            >
+                                New Organization
+                            </Button>
+                            <br />
+                            <Button
+                                color="primary"
+                                className={this.props.classes.button}
+                                onClick={() => this.props.history.push('/newEvent')}
+                            >
+                                New Event
+                            </Button>
+                            <br />
+                            <Button
+                                color="primary"
+                                className={this.props.classes.button}
+                                onClick={() => this.props.history.push('/newIndividual')}
+                            >
+                                New Individual
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </div>
                 <button onClick={() => this.props.history.push('/editOrganization')}>Sample Organization</button>
                 <button onClick={() => this.props.history.push('/newOrganization')}>Add New Organization</button>
                 <button onClick={() => this.props.history.push('/home')}>Home</button>
+
             </>
         )
     }
 }
 
-const mapStateToProps = state => ({
-    errors: state.errors,
-});
+const mapStateToProps = (reduxStore) => ({
+    reduxStore
+})
 
 export default connect(mapStateToProps)(withStyles(styles)(CollectBooks));
