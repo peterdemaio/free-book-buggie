40a3fea07fa8e701ec9ee56afc52ef3bebfcdd89
Nic Payne <nic.r.payne@gmail.com>
2020-03-09 16:31:35 -0500

    working MUI styling

--- src/components/NewOrganization/NewOrganization.js
+++ src/components/NewOrganization/NewOrganization.js
@@ -10,6 +10,64 @@ import NativeSelect from '@material-ui/core/NativeSelect';
 import InputBase from '@material-ui/core/InputBase';
 
 
+
+const BootstrapInput = withStyles(theme => ({
+    root: {
+        'label + &': {
+            marginTop: theme.spacing.unit * 3,
+        },
+    },
+    input: {
+        borderRadius: 4,
+        position: 'relative',
+        backgroundColor: theme.palette.background.paper,
+        border: '1px solid #ced4da',
+        fontSize: 16,
+        width: 'auto',
+        padding: '10px 26px 10px 12px',
+        transition: theme.transitions.create(['border-color', 'box-shadow']),
+        // Use the system font instead of the default Roboto font.
+        fontFamily: [
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(','),
+        '&:focus': {
+            borderRadius: 4,
+            borderColor: '#80bdff',
+            boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+        },
+    },
+}))(InputBase);
+
+const styles = theme => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    margin: {
+        margin: theme.spacing.unit,
+    },
+    bootstrapFormLabel: {
+        fontSize: 18,
+    },
+});
+
+
+
+
+
+
+
+
+
 class NewOrganization extends Component {
 
     state = {
@@ -25,9 +83,7 @@ class NewOrganization extends Component {
             county: '',
             zip: '',
             notes: ''
-
         },
-      
     }
 
     handleChangeFor = (event, propertyName) => {
@@ -36,7 +92,6 @@ class NewOrganization extends Component {
             newEntry: {
                 ...this.state.newEntry,
                 [propertyName]: event.target.value
-            
             }
         })
     }
@@ -50,18 +105,62 @@ class NewOrganization extends Component {
             })
         } catch {
             console.log('dispatch error')
-        }
-      
+        }   
     }
 
     render() {
 
         if (this.props.reduxStore.demographicsBoolean === true){
             alert('way to go, proud of you')
-          
         }
+
+        const { classes } = this.props;
+
         return (
             <>
+                <form className={classes.root} autoComplete="off">
+                    <FormControl className={classes.margin}>
+                        <InputLabel htmlFor="age-customized-select" className={classes.bootstrapFormLabel}>
+                            Age
+          </InputLabel>
+                        <BootstrapInput />
+                    </FormControl>
+                    <FormControl className={classes.margin}>
+                        <InputLabel htmlFor="age-customized-select" className={classes.bootstrapFormLabel}>
+                            Age
+          </InputLabel>
+                        <Select
+                            value={this.state.age}
+                            onChange={this.handleChange}
+                            input={<BootstrapInput name="age" id="age-customized-select" />}
+                        >
+                            <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem>
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <FormControl className={classes.margin}>
+                        <InputLabel htmlFor="age-customized-native-simple" className={classes.bootstrapFormLabel}>
+                            Age
+          </InputLabel>
+                        <NativeSelect
+                            value={this.state.age}
+                            onChange={this.handleChange}
+                            input={<BootstrapInput name="age" id="age-customized-native-simple" />}
+                        >
+                            <option value="" />
+                            <option value={10}>Ten</option>
+                            <option value={20}>Twenty</option>
+                            <option value={30}>Thirty</option>
+                        </NativeSelect>
+                    </FormControl>
+                </form>
+    );
+              }
+            }
                 <div>
                     <h1 align="center">Add New Organization</h1>
                     {/* <button onClick={() => this.props.history.push('/collectBooks')}>Add</button>
@@ -119,4 +218,11 @@ const mapStateToProps = (reduxStore) => ({
     reduxStore
 })
 
-export default connect(mapStateToProps)(NewOrganization);
+NewOrganization.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+let connectApp = connect(mapStateToProps)(NewOrganization)
+
+
+export default withStyles(styles)(connectApp);
