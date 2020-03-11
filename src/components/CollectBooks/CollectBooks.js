35452766af6552ec669968bdff8aad0043260a4c
AndyMcElligott <andymc2315@gmail.com>
2020-03-11 15:40:20 -0500

    brought in search

--- src/components/CollectBooks/CollectBooks.js
+++ src/components/CollectBooks/CollectBooks.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import OrganizationsListPage from '../OrganizationsListPage/OrganizationsListPage';
+import OrganizationsListItem from '../OrganizationsListItem/OrganizationsListItem';
 
 
 // Material UI imports
@@ -93,11 +93,26 @@ class CollectBooks extends Component {
         open: false,
     }
 
-    // componentDidMount() {
-    //     this.props.dispatch({
-    //         type: 'FETCH_USER'
-    //     })
-    // }
+    componentDidMount() {
+        this.props.dispatch({
+            type: 'UPDATE_ORGANIZATIONS',
+            payload: this.props.reduxStore.organizations
+        })
+    }
+
+    onInputChange = (event) => {
+        console.log(event.target.value)
+        let newlyDisplayed = this.props.reduxStore.organizations.filter( 
+            organization => organization.name.toLowerCase().includes(event.target.value.toLowerCase()) || 
+            organization.city.toLowerCase().includes(event.target.value.toLowerCase()) ||
+            organization.county.toLowerCase().includes(event.target.value.toLowerCase())
+            );
+
+        this.props.dispatch({
+            type: 'UPDATE_ORGANIZATIONS',
+            payload: newlyDisplayed
+        })
+    }
 
     // MUI Select controls
     handleClickOpen = () => {
@@ -112,6 +127,7 @@ class CollectBooks extends Component {
         this.setState({ [name]: Number(event.target.value) });
     };
 
+    // submit books event handler
     submitBooks = (event) => {
         console.log('adding books', this.state.numOfBooks);
         event.preventDefault();
@@ -138,6 +154,28 @@ class CollectBooks extends Component {
                 <h1>Collect Books</h1>
                 {/* Search functionality for orgs, make clickable to display info */}
                 {/* <OrganizationsListPage /> */}
+                <div>
+                    <span>Search for an organization by name, city or county: </span>
+
+                    <Input 
+                        className={this.props.classes.input}
+                        placeholder="search here "
+                        onChange={this.onInputChange}>
+                    </Input>
+                </div>
+                <div>
+                    <Grid container 
+                    className={this.props.classes.list}
+                    direction="column"
+                    justify="space-evenly"
+                    alignItems="left"
+                    >
+                        {this.props.reduxStore.updateOrganizations.map(org =>
+
+                            <OrganizationsListItem key={org.id} org={org} />
+                        )}
+                    </Grid>
+                </div>
 
 
                 <Grid container
