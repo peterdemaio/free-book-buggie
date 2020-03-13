76b4298f157b7ccff4a8abf78d48d011f62faa37
Nic Payne <nic.r.payne@gmail.com>
2020-03-13 12:03:25 -0500

    converted home page to class from functional component

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,24 +1,90 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
+
+//importing components for animated countup
+import { render } from 'react-dom';
+import CountUp, { startAnimation } from 'react-countup';
+// import VisibilitySensor from 'react-visibility-sensor';
+
+
+
 
 // this could also be written with destructuring parameters as:
 // const UserPage = ({ user }) => (
 // and then instead of `props.user.username` you could use `user.username`
-const UserPage = (props) => (
-  <div>
-    <h1 id="welcome">
-      Welcome, { props.user.username }!
+class UserPage extends Component {
+
+  componentDidMount() {
+    console.log('in componentDidMount')
+    
+    
+  }
+
+
+
+  render() {
+
+    return (
+      <div>
+        <h1 id="welcome">
+          Welcome, {this.props.user.username}!
     </h1>
-    <p>Your ID is: {props.user.id}</p>
-    <button onClick={() => props.history.push('/collectBooks')}>Collect Books</button>
-    <button onClick={() => props.history.push('/distributeBooks')}>Distribute Books</button>
-    <button onClick={() => props.history.push('/dataReporting')}>Data Reporting</button>
-    <br/>
-    <button onClick={() => props.history.push('/organizationsListPage')}>Organization List</button>
-    <LogOutButton className="log-in" />
-  </div>
-);
+
+        <p>Your ID is: {this.props.user.id}</p>
+        <button onClick={() => this.props.history.push('/collectBooks')}>Collect Books</button>
+        <button onClick={() => this.props.history.push('/distributeBooks')}>Distribute Books</button>
+        <button onClick={() => this.props.history.push('/dataReporting')}>Data Reporting</button>
+        <br />
+        <button onClick={() => this.props.history.push('/organizationsListPage')}>Organization List</button>
+        <LogOutButton className="log-in" />
+
+
+        <h1>
+          <CountUp
+            start={0}
+            end={18164}
+            duration={3}
+            separator=","
+            decimals={0}
+            decimal=","
+            prefix="Total books received to date: "
+            suffix=" "
+            onEnd={() => console.log('Ended! 👏')}
+            onStart={() => console.log('Started! 💨')}
+          >
+            {({ countUpRef, start }) => (
+              <div>
+                <span ref={countUpRef} />
+                <button onClick={start}>Start</button>
+              </div>
+            )}
+          </CountUp>
+          <CountUp
+            start={0}
+            end={12957}
+            duration={3}
+            separator=","
+            decimals={0}
+            decimal=","
+            prefix="Children impacted to date: "
+            suffix=" "
+            onEnd={() => console.log('Ended! 👏')}
+            onStart={() => console.log('Started! 💨')}
+          >
+            {({ countUpRef, start }) => (
+              <div>
+                <span ref={countUpRef} />
+                <button onClick={start}>Start</button>
+              </div>
+            )}
+          </CountUp>
+        </h1>
+
+      </div>
+    );
+  }
+}
 
 // Instead of taking everything from state, we just want the user info.
 // if you wanted you could write this code like this:
