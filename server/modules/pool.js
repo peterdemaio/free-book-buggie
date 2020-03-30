5fb27cd3d76624e1c20dc86a72fc3c9fc5d89926
Russell Kerber <russell.kerber@gmail.com>
2020-03-30 16:58:39 -0500

    test push

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -10,23 +10,23 @@ const url = require('url');
 
 let config = {};
 
-// if (process.env.DATABASE_URL) {
-//   // Heroku gives a url, not a connection object
-//   // https://github.com/brianc/node-pg-pool
-//   const params = url.parse(process.env.DATABASE_URL);
-//   const auth = params.auth.split(':');
+if (process.env.DATABASE_URL) {
+  // Heroku gives a url, not a connection object
+  // https://github.com/brianc/node-pg-pool
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
 
-//   config = {
-//     user: auth[0],
-//     password: auth[1],
-//     host: params.hostname,
-//     port: params.port,
-//     database: params.pathname.split('/')[1],
-//     ssl: true, // heroku requires ssl to be true
-//     max: 10, // max number of clients in the pool
-//     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-//   };
-// } else {
+  config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true, // heroku requires ssl to be true
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  };
+} else {
   config = {
     host: 'localhost', // Server hosting the postgres database
     port: 5432, // env var: PGPORT
@@ -34,7 +34,7 @@ let config = {};
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
-//}
+}
 
 // this creates the pool that will be shared by all other modules
 const pool = new pg.Pool(config);
